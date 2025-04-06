61b0b811a4004ed0b0d0ba0a76d34fe83d3f28db
Jing Li <thyrlian@gmail.com>
2025-04-07 01:46:59 +0200

    Center application window on screen

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <QApplication>
 #include <QMainWindow>
+#include <QScreen>
 
 int main(int argc, char *argv[])
 {
@@ -8,7 +9,16 @@ int main(int argc, char *argv[])
     QMainWindow mainWindow;
     mainWindow.setWindowTitle("LongView");
     mainWindow.resize(1024, 768);
+    
+    // Center the window on the screen
+    QScreen *screen = QGuiApplication::primaryScreen();
+    QRect screenGeometry = screen->geometry();
+    mainWindow.move(
+        (screenGeometry.width() - mainWindow.width()) / 2,
+        (screenGeometry.height() - mainWindow.height()) / 2
+    );
+    
     mainWindow.show();
     
     return app.exec();
-} 
+}
